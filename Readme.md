3388f49ded7d0133136c586d9aa59e6c95e6d306
Riadh-Hasan <Riadh.Hasan@brainstation-23.com>
2023-04-13 14:08:08 +0600

    Project Initialized

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# knowledge station 23
+A Book Store website which is build by Asp.Net Framework MVC pattern and for database i have used MySql database.
---
+++ knowledge station 23/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace knowledge_station_23.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
