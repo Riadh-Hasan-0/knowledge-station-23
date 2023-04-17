556c3a7843a1e7ee8c40709a21a63a3dd6e5f280
Riadh-Hasan <Riadh.Hasan@brainstation-23.com>
2023-04-17 12:01:15 +0600

    Book model updated

--- knowledge station 23/Models/Book.cs
+++ knowledge station 23/Models/Book.cs
@@ -5,9 +5,13 @@ namespace knowledge_station_23.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public String Title { get; set; }
         public string Description { get; set; }
+        [Required]
+        [Display(Name = "Select Author")]
         public string AuthorId { get; set; }
         public string Path { get; set; }
         [NotMapped]
