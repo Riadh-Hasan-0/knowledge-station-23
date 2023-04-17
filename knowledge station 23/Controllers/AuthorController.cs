c416489a70717837f733b05fa9eed01ea6c81cee
Riadh-Hasan <Riadh.Hasan@brainstation-23.com>
2023-04-17 16:46:16 +0600

    list view added and create view updated

--- knowledge station 23/Controllers/AuthorController.cs
+++ knowledge station 23/Controllers/AuthorController.cs
@@ -2,6 +2,7 @@
 using knowledge_station_23.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.Operations;
+using Microsoft.JSInterop.Implementation;
 using System;
 
 namespace knowledge_station_23.Controllers
@@ -9,34 +10,27 @@ namespace knowledge_station_23.Controllers
     public class AuthorController : Controller
     {
         private readonly ApplicationDbContext Db;
-        public AuthorController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment environment;
+
+        public AuthorController(ApplicationDbContext db,IWebHostEnvironment environment)
         {
             this.Db = db;
+            this.environment = environment;
         }
         public IActionResult Index()
         {
             IEnumerable<Author> objCategoryList = this.Db.AuthorList;
             return View(objCategoryList);
         }
-        //Get
-        public IActionResult Create()
+       
+       
+       
+        private string UploadImage(Author obj)
         {
-            return View();
-        }
-        //post
-       /* public IActionResult Create(Author obj)
-        {
-*//*            obj.Path = UploadImage(obj);
-*//*            Db.AuthorList.Add(obj);
-            Db.SaveChanges();
-            return RedirectToAction("Index");
-        }*/
-       /* private string UploadImage(Author obj)
-        {
-            *//*string uniqueFileName = string.Empty;
+            string uniqueFileName = string.Empty;
             if (obj.ImagePath != null)
             {
-                string uploadFolder = Path.Combine(environment.WebRootPath, "Image/");
+                string uploadFolder = Path.Combine(environment.WebRootPath, "Content/Image/Author/");
                 uniqueFileName = Guid.NewGuid().ToString() + "_" + obj.ImagePath.FileName;
                 string filePath = Path.Combine(uploadFolder, uniqueFileName);
                 using (var fileStream = new FileStream(filePath, FileMode.Create))
@@ -44,8 +38,29 @@ namespace knowledge_station_23.Controllers
                     obj.ImagePath.CopyTo(fileStream);
                 }
             }
-           */
-/*            return uniqueFileName;
-*//*        }*/
+
+            return uniqueFileName;
+        }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Author obj)
+        {
+            
+                obj.Path = UploadImage(obj);
+                Db.AuthorList.Add(obj);
+                Db.SaveChanges();
+                TempData["Success"] = "New Author created successfully";
+                return RedirectToAction("Index");
+            
+            TempData["Success"] = "failed to save";
+            return View(obj);
+        }
+        
     }
 }
---
+++ knowledge station 23/wwwroot/js/PreviewImage.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#ChoseImage').change(function (e) {
+
+
+
+        let url = $('#ChoseImage').val();
+        let ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+        if (ChoseImage.files && ChoseImage.files[0] && (ext == "gif" || ext == "jpg" || ext == "jfif" || ext == "png" || ext == "bmp")) {
+            let reader = new FileReader();
+            reader.onload = function () {
+                let output = document.getElementById('PreviewImage');
+                output.src = reader.result;
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        }
+    });
+});
