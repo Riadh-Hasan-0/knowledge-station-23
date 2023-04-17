27647e04f3a539a0d2cdf96c55188f0ef1faa40f
Riadh-Hasan <Riadh.Hasan@brainstation-23.com>
2023-04-17 12:23:47 +0600

    Author Controller added

---
+++ knowledge station 23/Controllers/AuthorController.cs
@@ -0,0 +1,20 @@
+﻿using knowledge_station_23.Data;
+using knowledge_station_23.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace knowledge_station_23.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext Db;
+        public AuthorController(ApplicationDbContext db)
+        {
+            this.Db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Author> objCategoryList = this.Db.AuthorList;
+            return View(objCategoryList);
+        }
+    }
+}
