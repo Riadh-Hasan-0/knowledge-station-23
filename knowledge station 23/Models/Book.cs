c001c01a562090c7c4d02ce2801089fd3982e7ba
Riadh-Hasan <Riadh.Hasan@brainstation-23.com>
2023-04-17 11:53:42 +0600

    Book and Author model added

---
+++ knowledge station 23/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace knowledge_station_23.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public string Description { get; set; }
+        public string AuthorId { get; set; }
+        public string Path { get; set; }
+        [NotMapped]
+        [Display(Name = "Choose Image")]
+        public IFormFile ImageFile { get; set; }
+
+
+    }
+}
