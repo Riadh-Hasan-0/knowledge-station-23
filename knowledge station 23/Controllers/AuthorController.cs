444ee89a36ac4af3e1c3ca47a2537abae98923b9
Riadh-Hasan <Riadh.Hasan@brainstation-23.com>
2023-04-17 13:50:35 +0600

    update book class

--- knowledge station 23/Controllers/AuthorController.cs
+++ knowledge station 23/Controllers/AuthorController.cs
@@ -1,6 +1,8 @@
 ﻿using knowledge_station_23.Data;
 using knowledge_station_23.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Operations;
+using System;
 
 namespace knowledge_station_23.Controllers
 {
@@ -15,6 +17,35 @@ namespace knowledge_station_23.Controllers
         {
             IEnumerable<Author> objCategoryList = this.Db.AuthorList;
             return View(objCategoryList);
+        }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //post
+        public IActionResult Create(Author obj)
+        {
+            var uniqueFileName = UploadImage(obj);
+            Db.AuthorList.Add(obj);
+            Db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        private string UploadImage(Author obj)
+        {
+            string uniqueFileName = string.Empty;
+            if (obj.ImagePath != null)
+            {
+                string uploadFolder = Path.Combine(environment.WebRootPath, "Image/");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + obj.ImagePath.FileName;
+                string filePath = Path.Combine(uploadFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    obj.ImagePath.CopyTo(fileStream);
+                }
+            }
+           
+            return uniqueFileName;
         }
     }
 }
